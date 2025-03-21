0ae962b264e85490b76b60d0db3549f8d971dabb
winey-dev <lsmin0703@gmail.com>
2025-03-21 09:59:59 +0900

    🔊 fixed default log format

--- formatter.go
+++ formatter.go
@@ -8,5 +8,5 @@ import (
 type Formatter func(t time.Time, level LogLevel, format string, args ...any) string
 
 func defaultFormatter(t time.Time, level LogLevel, format string, args ...any) string {
-	return fmt.Sprintf("%s [%5s] %s", t.In(time.Local).Format(time.TimeOnly), loglevelNames[level], fmt.Sprintf(format, args...))
+	return fmt.Sprintf("%s [%5s] %s", t.In(time.Local).Format(time.TimeOnly+".000"), loglevelNames[level], fmt.Sprintf(format, args...))
 }
